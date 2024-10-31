67dfc60aef2a612440becde2e81932027d5f0c5c
Sowdarjya <kolaysowdarjya@gmail.com>
2024-10-31 21:24:03 +0530

    created header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/home";
 import CryptoCurrencies from "./pages/CryptoCurrencies";
-
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/cryptocurrencies",
-    element: <CryptoCurrencies />,
-  },
-]);
+import News from "./pages/News";
 
 const App = () => {
   return (
     <>
       <Header />
-      <RouterProvider router={router} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cryptos" element={<CryptoCurrencies />} />
+        <Route path="/news" element={<News />} />
+      </Routes>
     </>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-  return <header>header</header>;
+  return (
+    <header className="navbar bg-[#0A0A0A]">
+      <div className="navbar-start text-2xl">CoinScope</div>
+      <nav className="navbar-end">
+        <ul className="flex items-center justify-between">
+          <li>
+            <NavLink to="/" className="p-2 text-xl mx-4">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/cryptos" className="p-2 text-xl mx-4">
+              Cryptos
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/news" className="p-2 text-xl mx-4">
+              News
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
