d92d5e346c4b7f9eec54055309449dc7eecd2c19
Sowdarjya <kolaysowdarjya@gmail.com>
2024-10-31 19:21:55 +0530

    created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/home";
+import CryptoCurrencies from "./pages/CryptoCurrencies";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/cryptocurrencies",
+    element: <CryptoCurrencies />,
+  },
+]);
 
 const App = () => {
   return (
     <>
-      <h1>APP</h1>
+      <Header />
+      <RouterProvider router={router} />
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import Header from "./components/Header.jsx";
-import App from "./App.jsx";
-import Home from "./pages/home.jsx";
-import CryptoCurrencies from "./pages/CryptoCurrencies.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/cryptocurrencies",
-    element: <CryptoCurrencies />,
-  },
-]);
+import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Header />
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
+    <App />
   </StrictMode>
 );
