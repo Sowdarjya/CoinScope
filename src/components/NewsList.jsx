c4da04ea6b106311bbcd3d755a8ca738e33305a9
Sowdarjya <kolaysowdarjya@gmail.com>
2024-11-13 21:14:02 +0530

    Added styled card news component to the newslist

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -5,7 +5,9 @@ const NewsList = () => {
 
   const fetchNewsData = async () => {
     try {
-      const res = await fetch(`https://api.coingecko.com/api/v3/news?id=doge`);
+      const res = await fetch(
+        `https://api.coingecko.com/api/v3/news?id=crypto`
+      );
       const data = await res.json();
       setNewsList(data.data);
     } catch (error) {
@@ -18,9 +20,30 @@ const NewsList = () => {
   }, []);
 
   return (
-    <div className="flex items-center justify-center">
+    <div className=" items-center justify-center">
       {newsList.length > 0 ? (
-        newsList.map((news) => <li key={news.id}> {news.title} </li>)
+        <div className="grid gap-4 grid-cols-3 grid-rows-3 p-4">
+          {newsList.map((news) => (
+            <div
+              className="card bg-base-100 w-96 shadow-xl"
+              key={news.updated_at}
+            >
+              <figure className="px-10 pt-10">
+                <img src={news.thumb_2x} className="rounded-xl h-[12rem]" />
+              </figure>
+              <div className="card-body items-center text-center">
+                <p>{news.title}</p>
+                <div className="card-actions">
+                  <button className="btn hover:bg-[#faed26] hover:text-[#121111]">
+                    <a href={news.url} target="_blank">
+                      Check out
+                    </a>
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       ) : (
         <span className="loading loading-ring loading-lg bg-[#faed26] h-screen"></span>
       )}
