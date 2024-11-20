3ae43394b42a635fca7fbfdd5618654308ae27ad
Sowdarjya <kolaysowdarjya@gmail.com>
2024-11-20 08:20:25 +0530

    Added search feature in coinlist

--- src/components/CoinList.jsx
+++ src/components/CoinList.jsx
@@ -6,11 +6,13 @@ import { Link } from "react-router-dom";
 
 const CoinList = () => {
   const [coinList, setCoinList] = useState([]);
+  const [filteredCoinList, setFilteredCoinList] = useState([]);
+  const [search, setSearch] = useState("");
 
   const fetchCoinList = async () => {
     try {
       const res = await fetch(
-        `https://api.coinranking.com/v2/coins?currency=usd&limit=99`
+        `https://api.coinranking.com/v2/coins?referenceCurrencyUuid=6mUvpzCc2lFo&limit=99`
       );
       const data = await res.json();
       setCoinList(data.data.coins);
@@ -22,52 +24,72 @@ const CoinList = () => {
   useEffect(() => {
     fetchCoinList();
   }, []);
+
+  useEffect(() => {
+    const filteredData = coinList.filter((coin) =>
+      coin.name.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilteredCoinList(filteredData);
+  }, [search]);
   return (
-    <div className="p-6">
-      {coinList.length > 0 ? (
-        <div className="grid grid-cols-3 gap-6 place-items-center">
-          {coinList.map((coin) => (
-            <Link
-              key={coin.uuid}
-              className="w-[95%] hover:scale-105 transform transition duration-300"
-            >
-              <div className="card bg-base-100 shadow-md rounded-lg overflow-hidden">
-                <div className="flex items-center p-4">
-                  <img
-                    src={coin.iconUrl}
-                    alt={coin.name}
-                    className="h-12 w-12 mr-4"
-                  />
-                  <div>
-                    <p className="text-lg font-bold">
-                      {coin.rank}. {coin.symbol}
-                    </p>
-                    <p className="text-lg ">Price: ${millify(coin.price)}</p>
-                    <p className="text-lg ">
-                      Market cap: ${millify(coin.marketCap)}
-                    </p>
-                    <p className="text-lg">
-                      24h change:{" "}
-                      <span
-                        className={
-                          coin.change >= 0 ? " text-lime-500" : " text-red-500"
-                        }
-                      >
-                        {millify(coin.change)}%
-                      </span>{" "}
-                    </p>
+    <>
+      <div className="flex items-center justify-center mt-4">
+        <input
+          type="text"
+          placeholder="Search cryptocurrency"
+          value={search}
+          className="w-1/2 p-3 outline-none bg-base-100 rounded-md"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      <div className="p-6">
+        {coinList.length > 0 ? (
+          <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-6 place-items-center">
+            {filteredCoinList.map((coin) => (
+              <Link
+                key={coin.uuid}
+                className="w-[95%] hover:scale-105 transform transition duration-300"
+              >
+                <div className="card bg-base-100 shadow-md rounded-lg overflow-hidden">
+                  <div className="flex items-center p-4">
+                    <img
+                      src={coin.iconUrl}
+                      alt={coin.name}
+                      className="h-12 w-12 mr-4"
+                    />
+                    <div>
+                      <p className="text-lg font-bold">
+                        {coin.rank}. {coin.symbol}
+                      </p>
+                      <p className="text-lg ">Price: ${millify(coin.price)}</p>
+                      <p className="text-lg ">
+                        Market cap: ${millify(coin.marketCap)}
+                      </p>
+                      <p className="text-lg">
+                        24h change:{" "}
+                        <span
+                          className={
+                            coin.change >= 0
+                              ? " text-lime-500"
+                              : " text-red-500"
+                          }
+                        >
+                          {millify(coin.change)}%
+                        </span>{" "}
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Link>
-          ))}
-        </div>
-      ) : (
-        <div className="flex justify-center items-center h-screen">
-          <span className="loading loading-ring loading-lg bg-[#faed26]"></span>
-        </div>
-      )}
-    </div>
+              </Link>
+            ))}
+          </div>
+        ) : (
+          <div className="flex justify-center items-center h-screen">
+            <span className="loading loading-ring loading-lg bg-[#faed26]"></span>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
